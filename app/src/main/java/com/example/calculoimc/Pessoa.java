903515f630d9c36334a53d62af0b8d648c5d7400
Adriel de Souza <adriel.2000@hotmail.com>
2019-06-10 10:52:16 -0300

    Adição da dataCadastro na pessoa e formatação da mesma.

--- app/src/main/java/com/example/calculoimc/Pessoa.java
+++ app/src/main/java/com/example/calculoimc/Pessoa.java
@@ -7,10 +7,12 @@ public class Pessoa {
     private double altura;
     private double peso;
     private double imc;
+    private double pesoIdeal;
     private String nome;
     private int idade;
     private String uuid;
-    private Date dataCadastro;
+    private String dataCadastro;
+    private SimpleDateFormat dtFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
     public String toString(){
         return "Nome: " + nome +
@@ -23,16 +25,15 @@ public class Pessoa {
 
     }
 
-    Pessoa(String uuid, String nome, String peso, String idade, String altura, String imc, long dataCadastro){
+    Pessoa(String uuid, String nome, double peso, int idade, double altura, double imc, long dataCadastro, double pesoIdeal){
         this.uuid = uuid;
         this.nome = nome;
-        this.imc = Double.parseDouble(imc);
-        this.peso = Double.parseDouble(peso);
-        this.altura = Double.parseDouble(altura);
-        this.idade = Integer.parseInt(idade);
-
-//        this.dataCadastro.
-        ;
+        this.imc = imc;
+        this.peso = peso;
+        this.altura = altura;
+        this.idade = idade;
+        this.dataCadastro = dtFormat.format(dataCadastro);
+        this.pesoIdeal = pesoIdeal;
     }
 
     public String getNome() {
@@ -77,7 +78,13 @@ public class Pessoa {
         return uuid;
     }
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
+    public double getPesoIdeal() { return pesoIdeal; }
+
+    public void setPesoIdeal(double pesoIdeal) { this.pesoIdeal = pesoIdeal; }
+
+    public String getDataCadastro() { return dataCadastro; }
+
+    public void setDataCadastro(String dataCadastro) { this.dataCadastro = dataCadastro; }
+
+    public void setUuid(String uuid) { this.uuid = uuid; }
 }
